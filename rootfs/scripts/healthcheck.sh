078f9e2847b4038351f56a62a5c4d554b2b57994
Mike Nye <mike.nye@gmail.com>
2021-03-06 21:32:36 +0800

    remove unused variables

--- rootfs/scripts/healthcheck.sh
+++ rootfs/scripts/healthcheck.sh
@@ -9,16 +9,6 @@ PFCLIENT_LOG_FILE=$(find /var/log/pfclient -type f -iname "pfclient-log_*.log" |
 
 EXITCODE=0
 
-# Healthy if a successful updated has been reported from now to this many seconds ago
-CHECK_PERIOD_SECONDS=300
-
-# Pull info from logs and perform calculations
-TIMESTAMP_NOW=$(date --utc +%s.%N)
-LASTLOG_UPDATES_SENT=$(grep --binary-files=text -P "Successfully sent \d+ aircraft updates across \d+ packets" "$PFCLIENT_LOG_FILE" | tail -1 | tr -s " ")
-LASTLOG_TIMESTAMP=$(date --utc --date="$(echo "$LASTLOG_UPDATES_SENT" | cut -d ' ' -f 1,2)" +%s.%N)
-LASTLOG_NUMBER_UPDATES=$(echo "$LASTLOG_UPDATES_SENT" | grep -oP "Successfully sent \K\d+")
-SUCCESSFUL_SENT_UPDATES=$(echo "($TIMESTAMP_NOW - $LASTLOG_TIMESTAMP) < $CHECK_PERIOD_SECONDS" | bc)
-
 # Ensure connection to beasthost
 if ! check_tcp4_connection_established ANY ANY "$(get_ipv4 "$BEASTHOST")" "$BEASTPORT"; then
   EXITCODE=1
