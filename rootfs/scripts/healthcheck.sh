16ed2af7b9055f40189da41397e11bd7bc4d9221
Mike <mike.nye@gmail.com>
2021-03-06 17:20:10 +0800

    remove healthchecks that are no longer valid

--- rootfs/scripts/healthcheck.sh
+++ rootfs/scripts/healthcheck.sh
@@ -20,17 +20,17 @@ LASTLOG_NUMBER_UPDATES=$(echo "$LASTLOG_UPDATES_SENT" | grep -oP "Successfully s
 SUCCESSFUL_SENT_UPDATES=$(echo "($TIMESTAMP_NOW - $LASTLOG_TIMESTAMP) < $CHECK_PERIOD_SECONDS" | bc)
 
 # check to make sure we've sent updates recently
-if [[ "$SUCCESSFUL_SENT_UPDATES" -ne 1 ]]; then
-    echo "No updates sent in past $CHECK_PERIOD_SECONDS seconds. UNHEALTHY"
-    EXITCODE=1
-else
-    if [[ "$LASTLOG_NUMBER_UPDATES" -lt 1 ]]; then
-        echo "No updates sent in past $CHECK_PERIOD_SECONDS seconds. UNHEALTHY"
-        EXITCODE=1
-    else
-        echo "$(echo "$LASTLOG_UPDATES_SENT" | cut -d ']' -f 2 | tr -s ' ' | cut -d ' ' -f 2-). HEALTHY"
-    fi
-fi
+#if [[ "$SUCCESSFUL_SENT_UPDATES" -ne 1 ]]; then
+#    echo "No updates sent in past $CHECK_PERIOD_SECONDS seconds. UNHEALTHY"
+#    EXITCODE=1
+#else
+#    if [[ "$LASTLOG_NUMBER_UPDATES" -lt 1 ]]; then
+#        echo "No updates sent in past $CHECK_PERIOD_SECONDS seconds. UNHEALTHY"
+#        EXITCODE=1
+#    else
+#        echo "$(echo "$LASTLOG_UPDATES_SENT" | cut -d ']' -f 2 | tr -s ' ' | cut -d ' ' -f 2-). HEALTHY"
+#    fi
+#fi
 
 # Ensure connection to beasthost
 if ! check_tcp4_connection_established ANY ANY "$(get_ipv4 "$BEASTHOST")" "$BEASTPORT"; then
