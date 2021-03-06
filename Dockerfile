15c4454879ed6a2a827f4e72060260e0b40ac4d5
Mike <mike.nye@gmail.com>
2021-03-06 22:03:45 +0800

    Image updates (#15)
    * update pfclient to 5.0.161
    
    * create directory structure
    
    * fix log dir ownership
    
    * create directory structures
    
    * improve build process & add healthchecks
    
    * improve healthcheck
    
    * improve healthchecks
    
    * linting
    
    * healthcheck updates
    
    * remove healthchecks that are no longer valid
    
    * update healthcheck
    
    * remove bc
    
    * remove unused variables
    
    * remove unused variable

--- Dockerfile
+++ Dockerfile
@@ -8,29 +8,51 @@ COPY rootfs/ /
 SHELL ["/bin/bash", "-o", "pipefail", "-c"]
 
 RUN set -x && \
+    TEMP_PACKAGES=() && \
+    KEPT_PACKAGES=() && \
+    # Deps for healthchecks
+    TEMP_PACKAGES+=(git) && \
+    KEPT_PACKAGES+=(jq) && \
+    KEPT_PACKAGES+=(net-tools) && \
+    KEPT_PACKAGES+=(procps) && \
+    # Deps for s6-overlay & pfclient install
+    TEMP_PACKAGES+=(file) && \
+    TEMP_PACKAGES+=(gnupg) && \
+    # Deps for pfclient
+    KEPT_PACKAGES+=(ca-certificates) && \
+    KEPT_PACKAGES+=(libc6) && \
+    KEPT_PACKAGES+=(lsb-base) && \
+    # Deps for s6-overlay, pfclient install & healthchecks
+    KEPT_PACKAGES+=(curl) && \
+    # Install packages
     apt-get update && \
-    apt-get install --no-install-recommends -y \
-        bash \
-        bc \
-        ca-certificates \
-        curl \
-        file \
-        libc6 \
-        lsb-base \
-        procps \
+    apt-get install -y --no-install-recommends \
+        ${KEPT_PACKAGES[@]} \
+        ${TEMP_PACKAGES[@]} \
         && \
+    # Install pfclient
     bash /scripts/install_pfclient.sh && \
-    mkdir -p /var/log/pfclient && \
-    chown -R nobody /var/log/pfclient && \
-    mkdir /config && \
-    chown -R nobody /config && \
     rm -rf /config/* /var/log/pfclient/* /etc/pfclient-config.json && \
-    touch /run/pfclient.pid && \
-    chown nobody /run/pfclient.pid && \
+    # Deploy healthchecks framework
+    git clone \
+      --depth=1 \
+      https://github.com/mikenye/docker-healthchecks-framework.git \
+      /opt/healthchecks-framework \
+      && \
+    rm -rf \
+      /opt/healthchecks-framework/.git* \
+      /opt/healthchecks-framework/*.md \
+      /opt/healthchecks-framework/tests \
+      && \
+    # Deploy s6-overlay
     curl -s https://raw.githubusercontent.com/mikenye/deploy-s6-overlay/master/deploy-s6-overlay.sh | sh && \
-    echo "pfclient $(pfclient --version | head -1 | rev | cut -d " " -f 1 | rev)" >> /VERSION && \
+    # Clean up
+    apt-get remove -y ${TEMP_PACKAGES[@]} && \
+    apt-get autoremove -y && \
     apt-get clean -y && \
     rm -rf /var/lib/apt/lists/* /src /tmp/* && \
+    # Container version
+    echo "pfclient $(pfclient --version | head -1 | rev | cut -d " " -f 1 | rev)" >> /VERSION && \
     grep 'pfclient' /VERSION | cut -d ' ' -f2- > /CONTAINER_VERSION
 
 ENTRYPOINT [ "/init" ]
--- rootfs/scripts/healthcheck.sh
+++ rootfs/scripts/healthcheck.sh
@@ -1,33 +1,49 @@
 #!/usr/bin/env bash
 
-set -e
-
-# Get latest log file
-PFCLIENT_LOG_FILE=$(find /var/log/pfclient -type f -iname "pfclient-log_*.log" | sort | tail -1)
+# Import healthchecks-framework
+# shellcheck disable=SC1091
+source /opt/healthchecks-framework/healthchecks.sh
 
 EXITCODE=0
 
-# Healthy if a successful updated has been reported from now to this many seconds ago
-CHECK_PERIOD_SECONDS=300
-
-# Pull info from logs and perform calculations
-TIMESTAMP_NOW=$(date --utc +%s.%N)
-LASTLOG_UPDATES_SENT=$(grep --binary-files=text -P "Successfully sent \d+ aircraft updates across \d+ packets" "$PFCLIENT_LOG_FILE" | tail -1 | tr -s " ")
-LASTLOG_TIMESTAMP=$(date --utc --date="$(echo "$LASTLOG_UPDATES_SENT" | cut -d ' ' -f 1,2)" +%s.%N)
-LASTLOG_NUMBER_UPDATES=$(echo "$LASTLOG_UPDATES_SENT" | grep -oP "Successfully sent \K\d+")
-SUCCESSFUL_SENT_UPDATES=$(echo "($TIMESTAMP_NOW - $LASTLOG_TIMESTAMP) < $CHECK_PERIOD_SECONDS" | bc)
-
-# check to make sure we've sent updates recently
-if [[ "$SUCCESSFUL_SENT_UPDATES" -ne 1 ]]; then
-    echo "No updates sent in past $CHECK_PERIOD_SECONDS seconds. UNHEALTHY"
-    EXITCODE=1
-else
-    if [[ "$LASTLOG_NUMBER_UPDATES" -lt 1 ]]; then
-        echo "No updates sent in past $CHECK_PERIOD_SECONDS seconds. UNHEALTHY"
-        EXITCODE=1
-    else
-        echo "$(echo "$LASTLOG_UPDATES_SENT" | cut -d ']' -f 2 | tr -s ' ' | cut -d ' ' -f 2-). HEALTHY"
-    fi
+# Ensure connection to beasthost
+if ! check_tcp4_connection_established ANY ANY "$(get_ipv4 "$BEASTHOST")" "$BEASTPORT"; then
+  EXITCODE=1
 fi
+
+# Check for abnormal service deaths
+if ! check_s6_service_abnormal_death_tally ALL; then
+  EXITCODE=1
+fi
+
+# Ensure data being sent to planefinder
+curl \
+  --location \
+  --silent \
+  'http://127.0.0.1:30053/ajax/stats' \
+  -X 'GET' \
+  -H 'Accept: application/json, text/javascript, */*; q=0.01' \
+  -H 'Pragma: no-cache' \
+  -H 'Cache-Control: no-cache' \
+  -H 'Accept-Encoding: gzip, deflate' \
+  -H 'Connection: keep-alive' \
+  -H 'X-Requested-With: XMLHttpRequest' \
+  > /run/pfclient_json_stats.current
+
+if [[ -e "/run/pfclient_json_stats.previous" ]]; then
+  
+  master_server_bytes_out_current="$(jq .master_server_bytes_out < /run/pfclient_json_stats.current)"
+  master_server_bytes_out_previous="$(jq .master_server_bytes_out < /run/pfclient_json_stats.previous)"
+
+  if [[ "$master_server_bytes_out_current" -gt "$master_server_bytes_out_previous" ]]; then
+    >&2 echo "Increase in bytes sent to master server. From $master_server_bytes_out_previous to $master_server_bytes_out_current. PASS"
+  else
+    >&2 echo "No increase in bytes sent to master server. From $master_server_bytes_out_previous to $master_server_bytes_out_current. FAIL"
+    EXITCODE=1
+  fi
+
+fi
+
+cp "/run/pfclient_json_stats.current" "/run/pfclient_json_stats.previous"
 
 exit $EXITCODE
